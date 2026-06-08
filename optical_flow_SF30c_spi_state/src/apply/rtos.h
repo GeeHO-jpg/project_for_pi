d0445e539fc9b36a7fc89910c6652c11f3e98e3d
GeeHO-jpg <chaiyawat630@gmail.com>
2026-06-08 09:21:26 +0700

    back to begin

--- optical_flow_SF30c_spi_state/src/apply/rtos.h
+++
@@ -1,9 +0,0 @@
-#pragma once 
-#include "../distance_sensor/TFMPlus/TFMPlus.h"
-
-
-#include "driver/i2c.h"
-
-
-void rtos_init();
-void lidar_init();
--- spi_cam_master_state/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "app/app_spi.h"
-
-int main() {
-    app_init();
-    while (true)
-        app_tick();
-    return 0;
-}
